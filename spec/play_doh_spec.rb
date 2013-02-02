dc6039a07e915bac81169ffbb83c13bc406b09b7
mike suarez <mikemps@gmail.com>
2013-02-02 11:50:04 -0500

    anything is a playdoh

--- spec/play_doh_spec.rb
+++ spec/play_doh_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 describe 'play-doh' do
 
   it 'is like a stub' do
+
+    #old school rr
     x = Object.new
     stub(x).anything
     x.anything.should be nil
+
+    #fresh & cool
+    Playdoh.new.anything.should be_a Playdoh
   end
 
 end
